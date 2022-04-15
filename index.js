83c31e597f0590860722eb1f5b8cccd526e7f628
Azat <harutyunyanazat29@gmail.com>
2022-04-16 01:42:15 +0400

    module type and oop style

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const puppeteer = require('puppeteer');
+import puppeteer from 'puppeteer'
+import ps from 'prompt-sync';
+const prompt = ps()
 
-(async () => {
-  const browser = await puppeteer.launch({headless: false});
-  const page = await browser.newPage();
-  await page.goto('https://www.instagram.com/accounts/login/');
-  await page.waitFor('input[name="username"]');
-  await page.focus('input[name="username"]');
-  await page.keyboard.type('fdkpdk11');
-  await page.focus('input[name="password"]');
-  await page.keyboard.type('azat123321');
-  await page.click('button[type="submit"]');
-})();
+class InstagramPage {
+	constructor(template) {
+		this._username = template.username
+		this._password = template.password
+	}
+
+	login() {
+		(async () => {
+			const browser = await puppeteer.launch({headless: false});
+			const page = await browser.newPage();
+			await page.goto('https://www.instagram.com/accounts/login/');
+			await page.waitFor('input[name="username"]');
+			await page.focus('input[name="username"]');
+			await page.keyboard.type(this._username);
+			await page.focus('input[name="password"]');
+			await page.keyboard.type(this._password);
+			await page.click('button[type="submit"]');
+		})();
+	}
+}
+
+const template = {
+	username: prompt("Please, enter your instagram username: "),
+	password: prompt("Please, enter your instagram password: ")
+}
+
+const myPage = new InstagramPage(template)
+myPage.login()

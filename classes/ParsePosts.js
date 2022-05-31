c9ccd10d19fdf325c3a29a9d6f1880f0dc55ac37
Azat <harutyunyanazat29@gmail.com>
2022-05-31 23:56:48 +0400

    posts parser

--- classes/ParsePosts.js
+++ classes/ParsePosts.js
@@ -38,44 +38,20 @@ class ParsePosts extends ExtendableParse {
 
         }, count)
 
-        console.log(allUrls)
+        let uniqueUrls = Array.from(new Set(allUrls)),
+        data = [];
 
-        // let posts_interval = setInterval(async () => {
-        //     score +=12
+        for (let i = 0; i < uniqueUrls.length; i++) {
+            data.push({
+                account: context._account,
+                postURL: uniqueUrls[i]
+            })
+        }
 
-        //     let allUrls = await page.evaluate((score, count, urls) => {
-        //         console.log(urls)
-
-        //         window.scrollBy(0, 1000)
-        //         Array.from(document.querySelectorAll('.v1Nh3 a')).forEach(post => {
-        //             urls.push(post)
-        //         })
-
-        //         if (score > count + 12) {
-        //             return urls
-        //         }
-                
-        //     }, score, count, urls)
-
-        //     if (score > count + 12) {
-        //         console.log(allUrls)
-        //         clearInterval(posts_interval)
-        //         let uniqueUrls = Array.from(new Set(allUrls)),
-        //         data = [];
-
-        //         for (let i = 0; i < uniqueUrls.length; i++) {
-        //             data.push({
-        //                 account: context._account,
-        //                 postURL: uniqueUrls[i]
-        //             })
-        //         }
-
-        //         console.log(data)
-        //         jobs.deleteMany({})
-        //         jobs.insertMany(data)
-        //         res()
-        //     }
-        // } ,1500)
+        console.log(data)
+        jobs.deleteMany({})
+        jobs.insertMany(data)
+        res()
     }
 }
 

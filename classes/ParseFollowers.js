d8f1eae76491d59b63e3847ac757853717c2f73c
Azat <harutyunyanazat29@gmail.com>
2022-06-02 14:28:03 +0400

    deleted unnecessary files

--- classes/ParseFollowers.js
+++ classes/ParseFollowers.js
@@ -10,3 +10,30 @@ class ParseFollowers extends ExtendableParse {
 }
 
 export const parseFollowers = new ParseFollowers()
+
+const funds = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
+];
+
+const arr = [
+    {
+        name: 'Alex',
+        salary: 500
+    },
+    {
+        name: 'Ann',
+        salary: 1500
+    },
+    {
+        name: 'John',
+        salary: 2500
+    },
+];
+ 
+const result = arr.map(item => Object.entries(item))
+console.log(result)
